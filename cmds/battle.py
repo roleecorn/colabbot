836963eeb0810ecd441d099b5d400dd86e0b2913
roleecorn <roleecorn@gmail.com>
2022-07-21 05:57:17 +0000

    rankChangeToDB

--- cmds/battle.py
+++ cmds/battle.py
@@ -71,25 +71,16 @@ class game(Cog_extension):
     @commands.command()
     @commands.cooldown(3, 1200, commands.BucketType.user)
     async def duel(self,ctx):
+        #確認是否跟自己對打
         with open(os.path.join("./data/", "waiting.csv"), newline='', encoding='UTF-8') as battelfile:
             waiting=battelfile.readline()
             
             battelfile.close()
-        
-        
-        
+
         if waiting == str(ctx.author.id):
             await ctx.send("不能跟自己對打喔")
             return
-        
-        
-        # with open(os.path.join("./data/", "pokemon.json"), newline='', encoding='UTF-8') as jsonfile:
-        #     pokemon = json.load(jsonfile)
-        #     jsonfile.close()
-        # if str(ctx.author.id) not in pokemon.keys():
-        #     await ctx.send("你沒有寶可夢，請先領一隻\n領取寶可夢方法:\n&&create 你的寶可夢的名字")
-        #     return
-        
+        #確認是否連線正常
         try:
             status = sqlite3.connect("/gdrive/My Drive/colabpractice/dcbot/data/pokemon.db")
             qry = f"SELECT * FROM pokemon where id={ctx.author.id} "
@@ -99,15 +90,10 @@ class game(Cog_extension):
                 await ctx.send("你沒有寶可夢，請先領一隻\n領取寶可夢方法:\n&&create 你的寶可夢的名字")
                 status.close()
                 return
-            # qry = f"SELECT * FROM pokemon where id={waiting} "
-            # dfd = pd.read_sql_query(qry, status)
-            
-            # status.close()
-
         except:
-            await ctx.channel.send("抱歉出了bug，請聯繫<@534243081135063041>來修正")
+            await ctx.send("抱歉出了bug，請聯繫<@534243081135063041>來修正")
             return
-        
+        #確認是否有人在排隊了，如果有就抓取資料並關閉資料庫連線
         if waiting == "1":
             with open(os.path.join("./data/", "waiting.csv"), newline='', encoding='UTF-8',mode='w') as battelfile:   
                 battelfile.write(str(ctx.author.id))
@@ -118,20 +104,17 @@ class game(Cog_extension):
             qry = f"SELECT * FROM pokemon where id={waiting} "
             dfd = pd.read_sql_query(qry, status)
             if(dfd.empty):
-                await ctx.channel.send("抱歉出了bug，請聯繫<@534243081135063041>來修正")
+                await ctx.send("抱歉出了bug，請聯繫<@534243081135063041>來修正")
                 return
             status.close()
         except:
-            await ctx.channel.send("抱歉出了bug，請聯繫<@534243081135063041>來修正")
+            await ctx.send("抱歉出了bug，請聯繫<@534243081135063041>來修正")
             return
         with open(os.path.join("./data/", "waiting.csv"), newline='', encoding='UTF-8',mode='w') as battelfile:
             battelfile.write("1")
             battelfile.close()
         
-        # pok1=pokemon[waiting][1]
-        # # pok1name=pokemon[waiting][0]
-        # pok2=pokemon[str(ctx.author.id)][1]
-        # # pok2name=pokemon[str(ctx.author.id)][0]
+        #換成pandas的形式
         try:
             poka=dfa.iloc[0]
             
@@ -140,7 +123,7 @@ class game(Cog_extension):
             pok2name=poka['name']
 
         except:
-            await ctx.channel.send("抱歉出了bug，請聯繫<@534243081135063041>來修正")
+            await ctx.send("抱歉出了bug，請聯繫<@534243081135063041>來修正")
         
         
         guild=self.bot.get_guild(689838165347139766)
@@ -159,8 +142,7 @@ class game(Cog_extension):
                 winer=pok1name
                 trainer=ctx.author
                 blackcar=challenger
-            # rank= changewin(str(blackcar))
-            # writeback("rank.json",rank)
+
             final(blackcar.id,trainer.id)
             descripebox=car(trainer.name,loser,winer)
             await channel.send(descripebox)
@@ -186,15 +168,13 @@ class game(Cog_extension):
             #waiting勝
             await channel.send(f"{pok1name}  獲得了勝利")
             print(f"winner is {waiting}")
-            # rank=changewin(waiting)
-            # writeback("rank.json",rank)
+
             final(int(waiting),ctx.author.id)
             return
         #ctx.author勝
         await channel.send(f"{pok2name}  獲得了勝利")
         print(f"winner is {ctx.author.id}")
-        # rank= changewin(str(ctx.author.id))
-        # writeback("rank.json",rank)
+
         final(ctx.author.id,int(waiting))
         return
 
--- cmds/showrk.py
+++ cmds/showrk.py
@@ -1,28 +1,39 @@
 import discord
 from discord.ext import commands
 from core.classes import Cog_extension
-import os
-from discord import File
-import json
-import random
+# import os
+# from discord import File
+# import json
+import sqlite3
+import pandas as pd
+
 class game(Cog_extension):
     @commands.command()
     async def rank(self,ctx):
-        with open(os.path.join("./data/", "rank.json"), newline='', encoding='UTF-8') as jsonfile:
-            rank = json.load(jsonfile)
-            jsonfile.close()
-        ranking=rank["rank"]
-        # guild=self.bot.get_guild(ctx.guild)
+
+        win_and_lose =sqlite3.connect("/gdrive/My Drive/colabpractice/dcbot/data/win_and_lose.db")
+        qry = f"SELECT id,win,lose FROM wl order by win DESC limit 10"
+        df = pd.read_sql_query(qry, win_and_lose)
+        win_and_lose.close()
+        
+        # with open(os.path.join("./data/", "rank.json"), newline='', encoding='UTF-8') as jsonfile:
+        #     rank = json.load(jsonfile)
+        #     jsonfile.close()
+        # ranking=rank["rank"]
+
         context=""
         for i in range(10):
-            tmp=ranking[str(i+1)]
-            member = await ctx.guild.fetch_member(int(tmp["id"]))
+            # tmp=ranking[str(i+1)]
+            # member = await ctx.guild.fetch_member(int(tmp["id"]))
+            member = await ctx.guild.fetch_member(int(df.iloc[i]["id"]))
             if member.nick==None:
                 context=context+f"第{i+1}位:"+str(member.name)
-                context=context+f" 勝場:{tmp['win']}\n"
-                
+                context=context+f" 勝場:{df.iloc[i]['win']}　"
+                context=context+f" 敗場:{df.iloc[i]['lose']}　\n"
                 continue
-            context=context+f"第{i+1}位:"+str(member.name)+f" 勝場:{tmp['win']}\n"
+            context=context+f"第{i+1}位:"+str(member.nick)
+            context=context+f" 勝場:{df.iloc[i]['win']}　"
+            context=context+f" 敗場:{df.iloc[i]['lose']} \n"
         await ctx.send(context)
 
 
